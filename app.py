abb9df63ea0d4098a47f5325aa64f1f637371019
insiya <insiya.h@consultadd.com>
2025-08-07 14:55:12 +0530

    code pushed

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ import numpy as np
 import boto3
 import os
 
-AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
-S3_BUCKET_NAME = os.getenv('S3_BUCKET_NAME')
-MODEL_FILE_KEY = os.getenv('S3_MODEL_KEY')
-MODEL_LOCAL_PATH = '/tmp/model.pkl' 
+AWS_ACCESS_KEY_ID = 'AKIA3UOYMTBEBHYZYKX4'
+AWS_SECRET_ACCESS_KEY = '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'
+S3_BUCKET_NAME = 'modellbuckett123'
+MODEL_FILE_KEY = 'house_price_model.pkl'
+MODEL_LOCAL_PATH = '/tmp/model.pkl'
 
 def load_model():
     """Download model from S3 and load."""
@@ -28,12 +28,10 @@ def load_model():
 app = Flask(__name__)
 model = load_model()
 
-# --- API Endpoints ---
 @app.route('/predict', methods=['GET'])
 def predict():
     if not model:
         return jsonify({'error': 'Model is not loaded'}), 500
-    
     try:
         data = request.get_json()
         features = [data['MedInc'], data['HouseAge'], data['AveRooms'], data['AveOccup']]
